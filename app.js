8609b71571d7dc54628e74c3c43d5f8019af5a39
Gage <gkolman17@gmail.com>
2022-09-13 14:26:02 -0700

    Completed assignment

--- app.js
+++ app.js
@@ -1,30 +1,110 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path');
+const JSON = require('json')
 const app = express()
 const port = 3000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.resolve(__dirname + '/client/dist')))
 var cors = require("cors");
 app.use(cors());
-const database = require('./database.js').table
+const database = require('./database.js').database
 
 app.use('*', (req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
 
-// app.get('/', function (req, res) {
+/*
+  takes a hash of {names: [name1, name2]} as the request body to get a list of doctors
+  if no list is provided all doctors will be retreived.
+*/
+app.get('/doctors', (req, res) => {
+  let body = JSON.stringify(req.body || {})
+    database.doctors.getDoctor(JSON.parse(body))
+    .then((doctors) => {
+      res.status(200).send(doctors)
+    })
+    .catch((error) => {
+      res.status(500).send(error)
+    })
+})
 
-//     res.write(__dirname + '/../client/dist')
-//     res.end()
-// });
+/*
+  takes a hash of {name: name} as the request body to add a new doctor.
+*/
+app.post('/doctors', (req, res) => {
+  let body = JSON.stringify(req.body || {})
+  database.doctors.addDoctor(JSON.parse(body))
+  .then(() => {
+    res.status(200).end()
+  })
+  .catch((error) => {
+    res.status(500).send(error)
+  })
+})
 
-// app.get('/', function(req, res){
-//     res.sendFile(path.resolve(__dirname+ '/../client/dist/index.html')); // change the path to your index.html
-// });
+/*
+  takes a hash of {name: name} as the request body to remove a new doctor.
+*/
+app.delete('/doctors', (req, res) => {
+  let = JSON.stringify(req.body)
+  database.doctors.removeDoctor(JSON.parse(body))
+  .then(() => {
+    res.status(200).end()
+  })
+  .catch((error) => {
+    res.status(500).send(error)
+  })
+})
+
+/*
+  takes a hash containing the properties of the appointments you wish to retreive
+  Ig {doctor: 'name', patient: 'name': date: 'dd/mm/yyyy', time: 'hh:mm', kind: 'kind' }.
+*/
+app.get('/appointments', (req, res) => {
+  let body = JSON.stringify(req.body || {})
+  database.appointments.getAppointment(JSON.parse(body))
+  .then((appointments) => {
+    res.status(200).send(appointments)
+  })
+  .catch((error) => {
+    res.status(500).send(error)
+  })
+})
+
+/*
+  takes a hash containing the properties of the appointments you wish to create
+  Ig {doctor: 'name', patient: 'name': date: 'dd/mm/yyyy', time: 'hh:mm', kind: 'kind' }.
+*/
+app.post('appointments', (req, res) => {
+  let body = JSON.stringify(req.body || {})
+  database.appointments.createAppointment(JSON.parse(body))
+    .then(() => {
+      res.status(200).end()
+    })
+    .catch((error) => {
+      res.status(500).send(error)
+    })
+})
+
+/*
+  takes a hash containing the properties of the appointments you wish to delete. Note all information
+  is required.Ig {doctor: 'name', patient: 'name': date: 'dd/mm/yyyy', time: 'hh:mm', kind: 'kind' }
+  in order to prevent deleting multiple records.
+*/
+app.delete('/appointments', (req, res) => {
+  let body = JSON.stringify(req.body)
+  database.appointments.createAppointment(JSON.parse(body))
+  .then(() => {
+    res.status(200).end()
+  })
+  .catch((error) => {
+    res.status(500).send(error)
+  })
+})
 
 app.listen(port, () => {
     console.log('Server started at http://localhost:' + port);
--- database.js
+++ database.js
@@ -1,47 +1,191 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const pass = process.env.MONGO_PASSWORD || 'no username'
-const user = process.env.MONGO_USERNAME || 'no password'
-const MONGO_ATLAS = `mongodb+srv://${user}:${pass}@cluster0.uvx8gel.mongodb.net/test`
-mongoose.connect(MONGO_ATLAS, {useNewUrlParser: true, useUnifiedTopology: true})
+const PASS = process.env.MONGO_PASSWORD || 'no username'
+const USER = process.env.MONGO_USERNAME || 'no password'
+let databaseURL;
+
+// pass = 'no password'
+if (PASS === 'no password') {
+  databaseURL = `mongodb://127.0.0.1:27017/`
+} else {
+  databaseURL = `mongodb+srv://${USER}:${PASS}@cluster0.uvx8gel.mongodb.net/test`
+}
+
+mongoose.connect(databaseURL, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => {console.log('mongo db connected')})
 .catch((err) => {console.log('mongo failed to connect ', err)})
 
-const newSchema = new mongoose.Schema({
-    name: String,
-    birthYear: Number,
+const appointment = new mongoose.Schema({
+    doctor: String,
+    patient: String,
+    kind: String,
+    date: String,
+    time: String
 });
 
-const Document = mongoose.model('table', newSchema);
+const doctor = new mongoose.Schema({
+  name: String
+});
 
+const Appointment = mongoose.model('Appointments', appointment);
+const Doctor = mongoose.model('Doctors', doctor);
 
-let createRecord = async (data) => {
-  let newDocument = new Document(data)
-  newDocument.save((error) => {
-    if (error) { console.log('error', error)}
-  })
-}
-let getRecord = (query) => {
-    return Document.find(query)
+let helpers = {
+  /*
+  Helper function used to validate appointment date.
+*/
+  validateDate: (date) => {
+    const errorMessage = `Invalid date, please check the date format again and make sure it follows the 
+                        "mm/dd/yyyy" format`
+    const dateSplit =  date.split('/')
+    try {
+      if (dateSplit.length !== 3 ) { throw new Error}
+      const [month, day, year] = dateSplit
+      if (parseInt(month) < 1 || parseInt(month) > 12) { throw new Error }
+      if (parseInt(day) < 1 || parseInt(day) > 31) { throw new Error }
+    } catch(error) {
+      throw new Error(errorMessage)
+    }
+
+  },
+
+  /*
+    Helper function used to validate appointment time.
+  */
+  validateTime: (time) => {
+    const errorMessage = `Invalid time, please check the time format again and make sure it follows the "hh:mm" 
+                        format, is in militarty time. I.G. 18:30 -> 6:30pm, and is in a 15 minute interval I.G
+                        08:15, 12:30`
+    const timeSplit =  time.split(':')
+    try {
+      if (timeSplit.length !== 2 ) { throw new Error }
+      const [hour, minute] = timeSplit
+      if (parseInt(hour) > 24) { throw new Error }
+      if (parseInt(minute) > 60 || parseInt(minute) % 15 !== 0) {  throw new Error }
+    } catch (error) {
+      throw new Error(errorMessage)
+    }
+  },
+  /*
+    Helper function used to validate appointment details.
+  */
+  validateAppointmentDetails: (details) => {
+    const requiredDetails = ['doctor', 'patient', 'kind', 'date', 'time']
+    for (var requiredDetail of requiredDetails) {
+      if (!details[requiredDetail]) {
+        throw new Error(`${requiredDetails} is required to make an appointment`) 
+      }
+    }
+  },
 }
 
-let deleteRecord = (id) => {
-    
-    return Document.find({id: id})
-    
-}
-let updateRecord = (id, updates) => {
-    
-    return Document.find({id: id})
-}
-// createRecord({name: 'Nicole', birthYear: 1995})
-// getRecord({$or: [{name: 'Becca'},{birthYear: 1996}]})
-// .then((data) => {
-//     console.log('data ->', data)
-// })
- module.exports = {
-    Document : Document
+let database = {
+  appointments:{
+    /*
+      Takes a hash containing some or all of the appointment details and returns an array containing
+      the apppointments that match the criteria.
+    */
+    getAppointment: async (details) => {
+      try {
+        return await Appointment.find(details)
+      } catch(error) {
+        console.log('failed to retrieve appointment(s)', error)
+      }
+    },
+
+    /*
+      Takes a hash containing all appointment details. Note, if an appointment detail id missing
+      such as a doctors name or patients name the appointment will not be created and an error will 
+      be raised.
+    */
+    createAppointment: async (details) => {
+      try {
+        helpers.validateAppointmentDetails(details)
+        helpers.validateTime(details.time)
+        helpers.validateDate(details.date)
+        const doctor = await Doctor.find({name: details.doctor})
+        if (doctor.length < 1) { throw new Error(`doctor ${details.doctor} is not in the database`)}
+        const doctorAppointmentsAtTime = await Appointment.find({date: details.date, time: details.time})
+        if (doctorAppointmentsAtTime.length >= 3) {
+          throw new Error(`${details.doctor} already has three appointments scheduled for this time`) 
+        }
+        await new Appointment(details).save()
+        console.log(`appointment created successfully`)
+      } catch(error) {
+        throw new Error(error)
+      }
+    },
+
+    /*
+      Takes hash containing all of the details to delete of an appointment to delete.
+    */
+    deleteAppointment: async (details) => {
+      try {
+        helpers.validateAppointmentDetails(details)
+        helpers.validateTime(details.time)
+        helpers.validateDate(details.date)
+        await Appointment.findOneAndRemove(details)
+        console.log(`successfully deleted appointment`)
+      } catch(error) {
+        throw new Error(`failed to cancel appointment ${error}`)
+      }
+    },
+  },
+  doctors : {
+    /*
+      Takes an array of hashes containing doctor names. Ig. {name: 'name'}.
+    */
+      getDoctor: async ({names}) => {
+      try {
+        if (!names.length < 1) {
+          return await Doctor.find()
+        } else {
+          const doctors = []
+          for (var name of names) {
+            const doctor = await Doctor.find({name: name})
+            doctors.push(doctor)
+          }
+          return doctors
+        }
+      } catch(error) {
+        throw new Error(`failed to get doctors(s) ${error}`)
+      }
+    },
+    /*
+      Takes a hash containing doctors name. Ig. {name: 'name'}.
+    */
+      addDoctor: async (doctor) => {
+      try {
+        await new Doctor(doctor).save()
+          console.log(`doctor ${doctor.name} has been added`)
+        } catch(error) {
+          throw new Error(`failed to add doctor ${error}`)
+      }
+    },
+    /*  
+      Takes a hash containing doctors name. Ig. {name: 'name'}. Throws an error if the 
+      doctor has scheduled appointments that need to be cancelled or rescheduled first.
+    */
+    removeDoctor: async(doctor) => {
+      
+      try {
+        if (!doctor.name) {throw new Error('No doctor for removal was specified')}
+        const DoctorsAppointments = await appointments.find({doctor: doctor.name})
+        if (DoctorsAppointments.length > 0) {
+          throw new Error('Doctor cannot be removed as they still have scheduled appointments');
+        }
+        await Doctor.findOneAndRemove(doctor)
+        console.log(`doctor ${doctor.name} has been removed`)
+        } catch(error) {
+          throw new Error(`failed to remove doctor ${error}`)
+      }
+    }
   }
+}
+
+ module.exports = {
+  database : database
+}
 
 
